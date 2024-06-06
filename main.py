b377d8b86c8e693d4ae67ab6d4d2f068d1c6f579
EnzoG14 <enzogonzalez1405@gmail.com>
2024-06-06 20:12:23 -0300

    probando sonar

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 var1="Hello World"
-
+var2=var1
+var3=var2
+var4=var3
 @app.get("/")
 async def root():
-    return {"message": var1}
+    return {"message": var4}
