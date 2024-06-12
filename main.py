437328e4b02f8aff522fb3340b764e1a41df590a
enzo <enzo@enzo.com>
2024-06-12 14:56:55 -0300

    Todo funcionando, espero

--- main.py
+++ main.py
@@ -4,18 +4,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-var1 = "Hello World" # Variable duplicada para simular código duplicado
+var1 = "Hello World"
 
 @app.get("/")
 async def root():
-    if True:
-        return {"message": var1}
-    else:
-        return {"message": var1}
+    return {"message": var1}
 
-47298475
 
-@app.get("/otra_ruta")
-async def otra_ruta():
-    return {"message": var1}  # Código duplicado
+
+#codigo para que me de error en sonar
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# var1 = "Hello World"
+
+# @app.get("/")
+# async def root():
+#     if True:
+#         return {"message": var1}
+#     else:
+#         return {"message": var1}
+
+# 47298475
+
+# @app.get("/otra_ruta")
+# async def otra_ruta():
+#     return {"message": var1}
 
