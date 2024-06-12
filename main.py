e4fa572b9282019db7964359c54824d42e0fd32d
enzo <enzo@enzo.com>
2024-06-12 15:04:44 -0300

    Ahora si todo funcionando

--- main.py
+++ main.py
@@ -10,26 +10,3 @@ var1 = "Hello World"
 async def root():
     return {"message": var1}
 
-
-
-#codigo para que me de error en sonar
-
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# var1 = "Hello World"
-
-# @app.get("/")
-# async def root():
-#     if True:
-#         return {"message": var1}
-#     else:
-#         return {"message": var1}
-
-# 47298475
-
-# @app.get("/otra_ruta")
-# async def otra_ruta():
-#     return {"message": var1}
-
